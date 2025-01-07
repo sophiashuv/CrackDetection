bcd3f4f683e614f3ccbf068a26084fbeef3bffd1
AdamHlg00 <adamoliver.holgersson@gmail.com>
2025-01-07 10:15:07 +0100

    testing some changes

--- custom_ml/layers/conv.py
+++ custom_ml/layers/conv.py
@@ -1,55 +1,82 @@
 import numpy as np
 
 class ConvLayer:
-    def __init__(self, input_channels, output_channels, kernel_size):
+    def __init__(self, input_channels, output_channels, kernel_size, stride=1, padding='same', use_batch_norm=False):
         self.kernel = np.random.randn(output_channels, input_channels, kernel_size, kernel_size) * np.sqrt(
             2.0 / (input_channels * kernel_size * kernel_size))
         self.bias = np.zeros((output_channels, 1))
+        self.stride = stride
+        self.padding = padding
+        self.use_batch_norm = use_batch_norm
+        if use_batch_norm:
+            self.gamma = np.ones((output_channels, 1))
+            self.beta = np.zeros((output_channels, 1))
+            self.running_mean = np.zeros((output_channels, 1))
+            self.running_variance = np.ones((output_channels, 1))
 
     def forward(self, x):
-        pad_h = (self.kernel.shape[2] - 1) // 2
-        pad_w = (self.kernel.shape[3] - 1) // 2
-        x_padded = np.pad(x, ((0, 0), (0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
+        pad_h = (self.kernel.shape[2] - 1) // 2 if self.padding == 'same' else 0
+        pad_w = (self.kernel.shape[3] - 1) // 2 if self.padding == 'same' else 0
+        self.input = np.pad(x, ((0, 0), (0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
 
-        self.input = x_padded
-        self.output = np.zeros((
-            x.shape[0],
-            self.kernel.shape[0],
-            x.shape[2],
-            x.shape[3]
-        ))
+        output_height = (self.input.shape[2] - self.kernel.shape[2]) // self.stride + 1
+        output_width = (self.input.shape[3] - self.kernel.shape[3]) // self.stride + 1
+        self.output = np.zeros((self.input.shape[0], self.kernel.shape[0], output_height, output_width))
 
-        for b in range(x.shape[0]):
+        for b in range(self.input.shape[0]):
             for c in range(self.kernel.shape[0]):
-                for i in range(self.output.shape[2]):
-                    for j in range(self.output.shape[3]):
+                for i in range(output_height):
+                    for j in range(output_width):
+                        start_i = i * self.stride
+                        start_j = j * self.stride
+                        end_i = start_i + self.kernel.shape[2]
+                        end_j = start_j + self.kernel.shape[3]
                         self.output[b, c, i, j] = np.sum(
-                            x_padded[b, :, i:i + self.kernel.shape[2], j:j + self.kernel.shape[3]] *
-                            self.kernel[c]
-                        ).item() + self.bias[c].item()
+                            self.input[b, :, start_i:end_i, start_j:end_j] * self.kernel[c]
+                        ) + self.bias[c]
+
+        if self.use_batch_norm:
+            self.mean = np.mean(self.output, axis=(0, 2, 3), keepdims=True)
+            self.variance = np.var(self.output, axis=(0, 2, 3), keepdims=True)
+            self.normalized_output = (self.output - self.mean) / np.sqrt(self.variance + 1e-7)
+            self.output = self.gamma * self.normalized_output + self.beta
+
         return self.output
 
     def backward(self, grad_output, learning_rate):
-        pad_h = (self.kernel.shape[2] - 1) // 2
-        pad_w = (self.kernel.shape[3] - 1) // 2
+        pad_h = (self.kernel.shape[2] - 1) // 2 if self.padding == 'same' else 0
+        pad_w = (self.kernel.shape[3] - 1) // 2 if self.padding == 'same' else 0
 
         grad_input = np.zeros_like(self.input)
         grad_kernel = np.zeros_like(self.kernel)
         grad_bias = np.zeros_like(self.bias)
 
+        if self.use_batch_norm:
+            grad_gamma = np.sum(grad_output * self.normalized_output, axis=(0, 2, 3), keepdims=True)
+            grad_beta = np.sum(grad_output, axis=(0, 2, 3), keepdims=True)
+            grad_output = grad_output * self.gamma / np.sqrt(self.variance + 1e-7)
+            grad_output -= (np.sum(grad_output, axis=(0, 2, 3), keepdims=True) +
+                            self.normalized_output * np.sum(grad_output * self.normalized_output, axis=(0, 2, 3),
+                                                            keepdims=True)) / (self.output.shape[2] * self.output.shape[3])
+
         for b in range(self.input.shape[0]):
             for c in range(self.kernel.shape[0]):
                 for i in range(grad_output.shape[2]):
                     for j in range(grad_output.shape[3]):
-                        grad_input[b, :, i:i + self.kernel.shape[2], j:j + self.kernel.shape[3]] += self.kernel[c] * \
-                                                                                                    grad_output[
-                                                                                                        b, c, i, j]
-                        grad_kernel[c] += self.input[b, :, i:i + self.kernel.shape[2], j:j + self.kernel.shape[3]] * \
-                                          grad_output[b, c, i, j]
+                        start_i = i * self.stride
+                        start_j = j * self.stride
+                        end_i = start_i + self.kernel.shape[2]
+                        end_j = start_j + self.kernel.shape[3]
+                        grad_input[b, :, start_i:end_i, start_j:end_j] += self.kernel[c] * grad_output[b, c, i, j]
+                        grad_kernel[c] += self.input[b, :, start_i:end_i, start_j:end_j] * grad_output[b, c, i, j]
                 grad_bias[c] += np.sum(grad_output[b, c])
 
         grad_input = grad_input[:, :, pad_h:-pad_h or None, pad_w:-pad_w or None]
 
         self.kernel -= learning_rate * grad_kernel
         self.bias -= learning_rate * grad_bias
+        if self.use_batch_norm:
+            self.gamma -= learning_rate * grad_gamma
+            self.beta -= learning_rate * grad_beta
+
         return grad_input
--- custom_ml/layers/maxpooling.py
+++ custom_ml/layers/maxpooling.py
@@ -1,9 +1,7 @@
-import numpy as np
-
-
-class MaxPoolLayer:
-    def __init__(self, pool_size):
+class PoolingLayer:
+    def __init__(self, pool_size, mode='max'):
         self.pool_size = pool_size
+        self.mode = mode  # Options: 'max', 'average'
 
     def forward(self, x):
         self.input = x
@@ -13,14 +11,21 @@ class MaxPoolLayer:
             x.shape[2] // self.pool_size,
             x.shape[3] // self.pool_size
         ))
-        self.max_indices = {}
+        self.indices = {}
+
         for b in range(x.shape[0]):
             for c in range(x.shape[1]):
                 for i in range(self.output.shape[2]):
                     for j in range(self.output.shape[3]):
-                        patch = x[b, c, i*self.pool_size:(i+1)*self.pool_size, j*self.pool_size:(j+1)*self.pool_size]
-                        self.output[b, c, i, j] = np.max(patch)
-                        self.max_indices[(b, c, i, j)] = np.unravel_index(np.argmax(patch), patch.shape)
+                        patch = x[b, c, i * self.pool_size:(i + 1) * self.pool_size,
+                                   j * self.pool_size:(j + 1) * self.pool_size]
+                        if self.mode == 'max':
+                            self.output[b, c, i, j] = np.max(patch)
+                            self.indices[(b, c, i, j)] = np.unravel_index(np.argmax(patch), patch.shape)
+                        elif self.mode == 'average':
+                            self.output[b, c, i, j] = np.mean(patch)
+                        else:
+                            raise ValueError(f"Unsupported pooling mode: {self.mode}")
         return self.output
 
     def backward(self, grad_output):
@@ -29,7 +34,12 @@ class MaxPoolLayer:
             for c in range(self.output.shape[1]):
                 for i in range(self.output.shape[2]):
                     for j in range(self.output.shape[3]):
-                        max_index = self.max_indices[(b, c, i, j)]
-                        grad_input[b, c, i*self.pool_size + max_index[0], j*self.pool_size + max_index[1]] = \
-                            grad_output[b, c, i, j]
+                        if self.mode == 'max':
+                            max_index = self.indices[(b, c, i, j)]
+                            grad_input[b, c, i * self.pool_size + max_index[0],
+                                       j * self.pool_size + max_index[1]] = grad_output[b, c, i, j]
+                        elif self.mode == 'average':
+                            grad_input[b, c, i * self.pool_size:(i + 1) * self.pool_size,
+                                       j * self.pool_size:(j + 1) * self.pool_size] += \
+                                grad_output[b, c, i, j] / (self.pool_size ** 2)
         return grad_input
