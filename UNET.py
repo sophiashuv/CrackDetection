e4a6a40038079f5acfdfc9b3708fad31d7105326
AdamHlg00 <adamoliver.holgersson@gmail.com>
2025-01-07 10:03:54 +0100

    Testing different UNET

--- UNET.py
+++ UNET.py
@@ -7,39 +7,51 @@ class UNETModel:
     def build(self):
         inputs = layers.Input(shape=self.input_shape)
 
-        c1 = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
-        c1 = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(c1)
+        # Encoder with additional convolutional layers
+        c1 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(inputs)
+        c1 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(c1)
         p1 = layers.MaxPooling2D((2, 2))(c1)
 
-        c2 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(p1)
-        c2 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(c2)
+        c2 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(p1)
+        c2 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(c2)
         p2 = layers.MaxPooling2D((2, 2))(c2)
 
-        c3 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(p2)
-        c3 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(c3)
+        c3 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(p2)
+        c3 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(c3)
         p3 = layers.MaxPooling2D((2, 2))(c3)
 
+        c4 = layers.Conv2D(512, (3, 3), activation="relu", padding="same")(p3)
+        c4 = layers.Conv2D(512, (3, 3), activation="relu", padding="same")(c4)
+        p4 = layers.MaxPooling2D((2, 2))(c4)
+
         # Bottleneck
-        c4 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(p3)
-        c4 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(c4)
+        c5 = layers.Conv2D(1024, (3, 3), activation="relu", padding="same")(p4)
+        c5 = layers.Conv2D(1024, (3, 3), activation="relu", padding="same")(c5)
 
-        # Decoder
-        u3 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same")(c4)
+        # Decoder with dropout layers
+        u4 = layers.Conv2DTranspose(512, (2, 2), strides=(2, 2), padding="same")(c5)
+        u4 = layers.concatenate([u4, c4])
+        c6 = layers.Conv2D(512, (3, 3), activation="relu", padding="same")(u4)
+        c6 = layers.Dropout(0.3)(c6)
+        c6 = layers.Conv2D(512, (3, 3), activation="relu", padding="same")(c6)
+
+        u3 = layers.Conv2DTranspose(256, (2, 2), strides=(2, 2), padding="same")(c6)
         u3 = layers.concatenate([u3, c3])
-        c5 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(u3)
-        c5 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(c5)
+        c7 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(u3)
+        c7 = layers.Dropout(0.3)(c7)
+        c7 = layers.Conv2D(256, (3, 3), activation="relu", padding="same")(c7)
 
-        u2 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same")(c5)
+        u2 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding="same")(c7)
         u2 = layers.concatenate([u2, c2])
-        c6 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(u2)
-        c6 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(c6)
+        c8 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(u2)
+        c8 = layers.Conv2D(128, (3, 3), activation="relu", padding="same")(c8)
 
-        u1 = layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding="same")(c6)
+        u1 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding="same")(c8)
         u1 = layers.concatenate([u1, c1])
-        c7 = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(u1)
-        c7 = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(c7)
+        c9 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(u1)
+        c9 = layers.Conv2D(64, (3, 3), activation="relu", padding="same")(c9)
 
-        outputs = layers.Conv2D(1, (1, 1), activation="sigmoid")(c7)
+        outputs = layers.Conv2D(1, (1, 1), activation="sigmoid")(c9)
 
         model = models.Model(inputs, outputs)
         return model
